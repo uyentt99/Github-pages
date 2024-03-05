4831fab7ba1c130b99264b781d9c88c33ca31d36
Uyen Tran <tranuyen0302@gmail.com>
2024-03-06 00:20:20 +0700

    Login form Example

---
+++ Sample/cpp/helo.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define task "a"
+#define Fu(i,a,b) for(int i=(a); i<=(b); ++i)
+#define Fd(i,a,b) for(int i=(a); i>=(b); --i)
+using namespace std;
+typedef long long LL;
+void pre(){
+    #ifndef ONLINE_JUDGE
+    freopen("a.inp", "r", stdin);
+    freopen("a.out", "w", stdout);
+    #endif // ONLINE_JUDGE
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+//
+int a[100];
+int main(){
+    pre();
+    int n;
+    cin>>n;
+    cout<<n;
+}
+
